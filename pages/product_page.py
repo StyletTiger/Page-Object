ab90aa55fd759d6c2ad0d17827d17d1216f8bc30
StyletTiger <stylettiger3004@gmail.com>
2023-10-23 18:57:28 +0300

    add

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,15 +2,22 @@ from pages.locators import ProductPageLocators
 
 
 class ProductPage():
-    def add_to_basket(self):
-        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
-        link.click()
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def add_product_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket.click()
+
+    #def should_be_product_added_message(self):
+        # проверка на соответствие
 
 
 
 
 
-
-
-
-# проверки на ожидаемый результат:
